ca9443032f1264052cb7e932881c2f31bfd8576d
sakshyam2002 <122495470+sakshyam2002@users.noreply.github.com>
2023-01-30 13:57:20 -0500

    question 2 complete

---
+++ src/main/java/org/example/Food.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Food {
+
+    private String name;
+
+    private String calories;
+
+    private int price;
+
+    private int protein;
+
+
+
+}
---
+++ src/main/java/org/example/Meal.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+public class Meal {
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
